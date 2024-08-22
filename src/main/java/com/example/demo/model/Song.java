5067ebceaa578ae3029916d840fe942173a2bfff
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-08-22 09:56:40 +0200

    Add Movie.java and Song.java classes

---
+++ src/main/java/com/example/demo/model/Song.java
@@ -0,0 +1,31 @@
+package com.example.demo.model;
+
+public class Song {
+	// instance variables
+	private Integer id;
+	private String title;
+	
+	// constructor
+	public Song(Integer id, String title) {
+		this.id = id;
+		this.title = title;
+	}
+	
+	// getters and setters
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+}
