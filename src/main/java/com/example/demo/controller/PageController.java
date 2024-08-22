32709c8ba1432ee4076c1777049628248d784a12
FusaroFederico <165822815+FusaroFederico@users.noreply.github.com>
2024-08-22 09:58:25 +0200

    Update PageController.java
    Add getBestMovies and getBestSongs private methods to PageController

--- src/main/java/com/example/demo/controller/PageController.java
+++ src/main/java/com/example/demo/controller/PageController.java
@@ -1,10 +1,16 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.model.Movie;
+import com.example.demo.model.Song;
 
 @Controller
 @RequestMapping("/")
@@ -15,5 +21,24 @@ public class PageController {
 		model.addAttribute("name", name);
 		return "homepage";
 	}
-
+	
+	private List<Movie> getBestMovies(){
+		List<Movie> movies = new ArrayList<Movie>();
+		movies.add(new Movie(0, "Il Signore Degli Anelli"));
+		movies.add(new Movie(1, "The Matrix"));
+		movies.add(new Movie(2, "Star Wars: L'Impero Colpisce Ancora"));
+		movies.add(new Movie(3, "Il Re Leone"));
+		movies.add(new Movie(4, "The Truman Show"));
+		return movies;
+	}
+	
+	private List<Song> getBestSongs(){
+		List<Song> songs = new ArrayList<Song>();
+		songs.add(new Song(0, "Imagine"));
+		songs.add(new Song(1, "Hey Jude"));
+		songs.add(new Song(2, "Billie Jean"));
+		songs.add(new Song(3, "Bohemian Rhapsody"));
+		songs.add(new Song(4, "Heroes"));
+		return songs;
+	}
 }
